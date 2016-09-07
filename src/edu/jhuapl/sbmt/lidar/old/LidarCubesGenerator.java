6eff22e40b81d9a1545c5f53f4740d7525b31bbe
Russell Turner <russell.turner@jhuapl.edu>
2016-09-07 09:14:23 -0400

    changed all variables and parameters of type "ArrayList" to "List"

--- src/edu/jhuapl/sbmt/lidar/old/LidarCubesGenerator.java
+++ src/edu/jhuapl/sbmt/lidar/old/LidarCubesGenerator.java
@@ -11,7 +11,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.List;
 import java.util.TreeSet;
 import java.util.zip.GZIPInputStream;
 
@@ -96,7 +96,7 @@ abstract public class LidarCubesGenerator
 
         new File(outputFolder).mkdirs();
 
-        ArrayList<String> lidarFiles = null;
+        List<String> lidarFiles = null;
         try
         {
             lidarFiles = FileUtil.getFileLinesAsStringList(lidarFileList);
