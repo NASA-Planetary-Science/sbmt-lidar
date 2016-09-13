8b7d0c89717961ea894ddb532342b616650d2a30
Mike Zimmerman <Michael.Zimmerman@jhuapl.edu>
2016-09-13 11:29:33 -0400

    Fixed a couple of minor issues relating to last commit.

--- src/edu/jhuapl/sbmt/lidar/hyperoctree/mola/MolaInputFile.java
+++ src/edu/jhuapl/sbmt/lidar/hyperoctree/mola/MolaInputFile.java
@@ -2,6 +2,7 @@ package edu.jhuapl.sbmt.lidar.hyperoctree.mola;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.Scanner;
 
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
@@ -41,6 +42,7 @@ public class MolaInputFile
         String[] tokens=line.trim().split("\\s+");   // split on whitespace, cf. http://stackoverflow.com/questions/225337/how-do-i-split-a-string-with-any-whitespace-chars-as-delimiters
         if (tokens.length==0)
             return null;
+        System.out.println(Arrays.toString(tokens));
         double lon=Double.valueOf(tokens[0]);   // degrees
         double lat=Double.valueOf(tokens[1]);   // degrees
         double r=Double.valueOf(tokens[2])/1000;            // km
@@ -49,8 +51,11 @@ public class MolaInputFile
         double intensity=0;
         //
         double[] xyz = MathUtil.latrec(new LatLon(lat/180.*Math.PI, lon/180.*Math.PI, r));
-        Vector3D tgpos=new Vector3D(xyz[1],xyz[0],xyz[2]);
+        Vector3D tgpos=new Vector3D(xyz[0],xyz[1],xyz[2]);
         //
+
+        System.out.println(range);
+
         return new MolaFSHyperPoint(tgpos.getX(), tgpos.getY(), tgpos.getZ(), time, intensity, range, fileNum);
 
     }
