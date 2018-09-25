15da591f7ffa286e7f0966bc41c802baedc1fd66
Colleen <colleen.oshea@jhuapl.edu>
2018-09-25 15:39:21 -0400

    switch to hayabusa specific condensor

--- src/edu/jhuapl/sbmt/lidar/hyperoctree/laser/Hayabusa2LaserFSHyperTreeCondenser.java
+++ src/edu/jhuapl/sbmt/lidar/hyperoctree/laser/Hayabusa2LaserFSHyperTreeCondenser.java
@@ -1,6 +1,7 @@
 package edu.jhuapl.sbmt.lidar.hyperoctree.laser;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import edu.jhuapl.sbmt.lidar.hyperoctree.FSHyperTreeCondenser;
 
@@ -12,6 +13,18 @@ public class Hayabusa2LaserFSHyperTreeCondenser extends FSHyperTreeCondenser
         super(rootPath, outFilePath);
     }
 
+    public static void main(String[] args)
+    {
+        // assumes this is being run from misc/scripts/generate_spectra_hypertree.sh
+        // so the temp_hypertree/ directory has been created in working directory.  TODO
+        Path rootPath=Paths.get("test_hypertree/");
+        Path outFilePath=rootPath.resolve("dataSource.lidar");
+        System.out.println("Root path = "+rootPath);
+        System.out.println("Output path = "+outFilePath);
+        Hayabusa2LaserFSHyperTreeCondenser condenser=new Hayabusa2LaserFSHyperTreeCondenser(rootPath,outFilePath);
+        condenser.condense();
+        System.out.println("Wrote tree structure to "+outFilePath);
+    }
 
     @Override
     public int getDimension()
