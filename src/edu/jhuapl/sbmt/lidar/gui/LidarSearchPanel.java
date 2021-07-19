1842ce2c0938fe6732ca0fa84774ac512800d693
Josh Steele <josh.steele@jhuapl.edu>
2021-07-18 21:58:48 -0400

    RM #1215 - Initial updates to support multiple bodies

--- src/edu/jhuapl/sbmt/lidar/gui/LidarSearchPanel.java
+++ src/edu/jhuapl/sbmt/lidar/gui/LidarSearchPanel.java
@@ -275,7 +275,7 @@ public class LidarSearchPanel extends JPanel
 	private void doActionClearRegion()
 	{
 		AbstractEllipsePolygonModel selectionModel = (AbstractEllipsePolygonModel) refModelManager
-				.getModel(ModelNames.CIRCLE_SELECTION);
+				.getModel(ModelNames.CIRCLE_SELECTION).get(0);
 		selectionModel.removeAllStructures();
 	}
 
@@ -314,7 +314,7 @@ public class LidarSearchPanel extends JPanel
 		refPickManager.setActivePicker(null);
 
 		AbstractEllipsePolygonModel selectionRegion = (AbstractEllipsePolygonModel) refModelManager
-				.getModel(ModelNames.CIRCLE_SELECTION);
+				.getModel(ModelNames.CIRCLE_SELECTION).get(0);
 		// Delegate actual query submission
 		handleActionSubmit(aDataSource, selectionRegion);
 	}
