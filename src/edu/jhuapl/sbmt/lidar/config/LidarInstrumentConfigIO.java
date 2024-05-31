a62e53edc378f5b56e82ec098437eee6a9524a6a
James Peachey <James.Peachey@jhuapl.edu>
2024-05-31 12:12:01 -0400

    sbmt/sbmt-lidar#62: get metadata code from jsqrl.

--- src/edu/jhuapl/sbmt/lidar/config/LidarInstrumentConfigIO.java
+++ src/edu/jhuapl/sbmt/lidar/config/LidarInstrumentConfigIO.java
@@ -8,10 +8,10 @@ import edu.jhuapl.sbmt.core.body.BodyViewConfig;
 import edu.jhuapl.sbmt.core.config.BaseFeatureConfigIO;
 import edu.jhuapl.sbmt.core.config.Instrument;
 
-import crucible.crust.metadata.api.Key;
-import crucible.crust.metadata.api.Metadata;
-import crucible.crust.metadata.api.Version;
-import crucible.crust.metadata.impl.SettableMetadata;
+import edu.jhuapl.ses.jsqrl.api.Key;
+import edu.jhuapl.ses.jsqrl.api.Metadata;
+import edu.jhuapl.ses.jsqrl.api.Version;
+import edu.jhuapl.ses.jsqrl.impl.SettableMetadata;
 
 public class LidarInstrumentConfigIO extends BaseFeatureConfigIO //extends BaseInstrumentConfigIO implements MetadataManager
 {
